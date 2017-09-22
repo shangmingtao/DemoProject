b07c6bf3d280c4be809660025e423f67ffd30dd9
shangmingtao <shangmingtao@126.com>
2017-09-22 17:35:55 +0800

    Update UserServiceImpl.java

--- dubbodemo-user-service/src/main/java/cn/milo/dubboserviceImpl/UserServiceImpl.java
+++ dubbodemo-user-service/src/main/java/cn/milo/dubboserviceImpl/UserServiceImpl.java
@@ -19,7 +19,7 @@ public class UserServiceImpl implements UserService{
         tUser user = new tUser();
         user.setUsernama(username);
         if (username.equals("milo")){
-            user.setName("hello milo");
+            user.setName("hello milo1");
         }else{
             user.setName("hello guest");
         }
