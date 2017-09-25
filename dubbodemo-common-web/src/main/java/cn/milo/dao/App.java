1044ea0a041126c3ae500c3a46bb9a6f7196e619
shangmingtao <shangmingtao@126.com>
2017-09-25 16:59:41 +0800

    Update App.java

--- dubbodemo-common-web/src/main/java/cn/milo/dao/App.java
+++ dubbodemo-common-web/src/main/java/cn/milo/dao/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello12222 World!" );
+        System.out.println( "Hello12222 World111!" );
     }
 }
